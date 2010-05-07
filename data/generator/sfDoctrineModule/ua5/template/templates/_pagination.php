0240282b32123c5de4cd6ac7d0c033ed6269fff5
Ken Marfilla <marfillaster@gmail.com>
2010-05-07 14:08:10 +0000

    Fixed pagination img src

--- data/generator/sfDoctrineModule/ua5/template/templates/_pagination.php
+++ data/generator/sfDoctrineModule/ua5/template/templates/_pagination.php
@@ -1,10 +1,10 @@
 <div class="ua5_admin_pagination">
   <a href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=1">
-    [?php echo image_tag(sfConfig::get('ua5_admin_module_web_dir').'/images/first.png', array('alt' => __('First page', array(), 'ua5_admin'), 'title' => __('First page', array(), 'ua5_admin'))) ?]
+    [?php echo image_tag(sfConfig::get('ua5_admin_module_web_dir', '/ua5AdminThemePlugin').'/images/first.png', array('alt' => __('First page', array(), 'ua5_admin'), 'title' => __('First page', array(), 'ua5_admin'))) ?]
   </a>
 
   <a href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $pager->getPreviousPage() ?]">
-    [?php echo image_tag(sfConfig::get('ua5_admin_module_web_dir').'/images/previous.png', array('alt' => __('Previous page', array(), 'ua5_admin'), 'title' => __('Previous page', array(), 'ua5_admin'))) ?]
+    [?php echo image_tag(sfConfig::get('ua5_admin_module_web_dir', '/ua5AdminThemePlugin').'/images/previous.png', array('alt' => __('Previous page', array(), 'ua5_admin'), 'title' => __('Previous page', array(), 'ua5_admin'))) ?]
   </a>
 
   [?php foreach ($pager->getLinks() as $page): ?]
@@ -16,10 +16,10 @@
   [?php endforeach; ?]
 
   <a href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $pager->getNextPage() ?]">
-    [?php echo image_tag(sfConfig::get('ua5_admin_module_web_dir').'/images/next.png', array('alt' => __('Next page', array(), 'ua5_admin'), 'title' => __('Next page', array(), 'ua5_admin'))) ?]
+    [?php echo image_tag(sfConfig::get('ua5_admin_module_web_dir', '/ua5AdminThemePlugin').'/images/next.png', array('alt' => __('Next page', array(), 'ua5_admin'), 'title' => __('Next page', array(), 'ua5_admin'))) ?]
   </a>
 
   <a href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $pager->getLastPage() ?]">
-    [?php echo image_tag(sfConfig::get('ua5_admin_module_web_dir').'/images/last.png', array('alt' => __('Last page', array(), 'ua5_admin'), 'title' => __('Last page', array(), 'ua5_admin'))) ?]
+    [?php echo image_tag(sfConfig::get('ua5_admin_module_web_dir', '/ua5AdminThemePlugin').'/images/last.png', array('alt' => __('Last page', array(), 'ua5_admin'), 'title' => __('Last page', array(), 'ua5_admin'))) ?]
   </a>
 </div>
