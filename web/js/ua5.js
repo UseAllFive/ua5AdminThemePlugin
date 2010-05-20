7fe58a113161c2f01419b29615d51a008a9be87d
J.J. Kaye <jjkaye@useallfive.com>
2010-05-20 17:56:55 +0000

    + Updated filter toggle JS

--- web/js/ua5.js
+++ web/js/ua5.js
@@ -11,6 +11,7 @@ UA5.Filter = Class.$extend({
   __init__ : function($_sel) {
   
     this.filter = $_sel;
+    this.open = false;
     
     if($($_sel)) {
     
@@ -23,7 +24,22 @@ UA5.Filter = Class.$extend({
   
   addToggle : function() {
   
-    var toggle = $('<a/>').attr({ 'class' : 'ua5_admin_filter_toggle' }).html('Open Filter');
+    var $self = this;
+  
+    var toggle = $('<a/>')
+      .attr({ 'class' : 'ua5_admin_filter_toggle' })
+      .html('Open Filter')
+      .click(function(e){
+      
+        e.preventDefault();
+    
+        if($self.open) {
+          $($self.filter + ' form').hide(); $(e.target).html('Open Filter'); $self.open = false;
+        } else {
+          $($self.filter + ' form').show(); $(e.target).html('Close Filter'); $self.open = true;
+        }
+    
+      });
     
     $(this.filter).append(toggle);
   
