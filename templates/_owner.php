5282ec8739559ace44b9c49104d234642757d2ce
J.J. Kaye <jjkaye@useallfive.com>
2010-06-28 20:08:57 +0000

    Updates to UA5 Admin header

--- templates/_owner.php
+++ templates/_owner.php
@@ -1,4 +1,4 @@
 <div id="hd-title">
-  <span class="account"><a href="/"><?php echo sfConfig::get("app_company_name"); ?></a></span>
-  <span class="client"><a href="/"><?php echo $sf_user->getGuardUser()->Profile->publication ?></a></span>
+  <span class="account"><a href="<?php echo url_for('@homepage') ?>"><?php echo sfConfig::get("app_company_name"); ?></a></span>
+  <span class="client"><span><?php echo sfConfig::get("app_product"); ?></span></span>
 </div>
