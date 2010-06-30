8d21ad5374c1d5927effe074f0f8716b44b24090
Alex Sidorov <asidorov01@gmail.com>
2010-06-30 14:17:18 +0000

    PressMS. Ticket #48

--- data/generator/sfDoctrineModule/ua5/parts/filtersAction.php
+++ data/generator/sfDoctrineModule/ua5/parts/filtersAction.php
@@ -1,14 +1,6 @@
   protected function getFilters()
   {
-    if (isset($this->overridden_filters))
-    {
-      sfContext::getInstance()->set("<?php echo $this->getModuleName() ?>.filters", $this->overridden_filters);    
-      return $this->overridden_filters;
-    }else
-    {
-      $filter_array = $this->getUser()->getAttribute('<?php echo $this->getModuleName() ?>.filters', $this->configuration->getFilterDefaults(), 'admin_module');
-    }
-    return $filter_array;
+    return $this->getUser()->getAttribute('<?php echo $this->getModuleName() ?>.filters', $this->configuration->getFilterDefaults(), 'admin_module');
   }
 
   protected function setFilters(array $filters)
