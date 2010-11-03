4013a8b7baa58e40312534e7eb4bc64d94f4075d
Alex Sidorov <asidorov01@gmail.com>
2010-11-03 02:22:21 +0000

    Anonymous. Ticket #117  - added credits setting for restrict tabs by permissions.

--- templates/_tabs.php
+++ templates/_tabs.php
@@ -5,9 +5,11 @@
 		$active = sfContext::getInstance()->get("active_tab");
 	}
   foreach(sfContext::getInstance()->get("tabs") as $record_key => $tabs_record){
+  	if(isset($tabs_record['credits']) && $sf_user->hasSfPermissions($tabs_record['credits'])):
 ?>
   <li class="tab"><a <?php if ($active == $record_key) {echo "class='active'";}?> href="<?php echo url_for($tabs_record['url'])?>"><?php echo $tabs_record['text']?></a></li>
 <?php 
+endif;
   }
 }?>
 </ul> 
