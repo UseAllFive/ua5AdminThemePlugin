5d60722f4edfee0cfc69a3df3f6181e5a17b772b
Matt Farmer <matt@useallfive.com>
2011-05-03 00:09:35 +0000

    + Fix boolean image path

--- data/generator/sfDoctrineModule/ua5/template/templates/_list_field_boolean.php
+++ data/generator/sfDoctrineModule/ua5/template/templates/_list_field_boolean.php
@@ -1,5 +1,5 @@
 [?php if ($value): ?]
-  [?php echo image_tag(sfConfig::get('ua5_admin_module_web_dir').'/images/tick.png', array('alt' => __('Checked', array(), 'ua5_admin'), 'title' => __('Checked', array(), 'ua5_admin'))) ?]
+  [?php echo image_tag(sfConfig::get('ua5_admin_module_web_dir', '/ua5AdminThemePlugin').'/images/tick.png', array('alt' => __('Checked', array(), 'ua5_admin'), 'title' => __('Checked', array(), 'ua5_admin'))) ?]
 [?php else: ?]
   &nbsp;
 [?php endif; ?]
