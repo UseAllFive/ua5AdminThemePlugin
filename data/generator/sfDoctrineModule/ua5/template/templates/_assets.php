992011a6e6d6db26dc01db6ce5153bd8cb5e81eb
Matt Farmer <matt@useallfive.com>
2011-05-09 23:03:21 +0000

    + Upgrade jQuery to 1.6 and include jQueryUI

--- data/generator/sfDoctrineModule/ua5/template/templates/_assets.php
+++ data/generator/sfDoctrineModule/ua5/template/templates/_assets.php
@@ -1,17 +1,18 @@
-[?php use_stylesheet('http://yui.yahooapis.com/2.8.0r4/build/reset-fonts-grids/reset-fonts-grids.css', 'first') ?]  
+[?php use_stylesheet('http://yui.yahooapis.com/2.8.0r4/build/reset-fonts-grids/reset-fonts-grids.css', 'first') ?]
 [?php use_stylesheet('<?php echo sfConfig::get('ua5_admin_module_web_dir', '/ua5AdminThemePlugin').'/css/admin.css' ?>', 'first') ?]
 
 
-[?php use_javascript('http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js', 'first') ?]
+[?php use_javascript('//ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.js', 'first') ?]
+[?php use_javascript('//ajax.googleapis.com/ajax/libs/jqueryui/1.8.12/jquery-ui.min.js', 'first') ?]
 [?php use_javascript('<?php echo sfConfig::get('ua5_admin_module_web_dir', '/ua5AdminThemePlugin').'/js/classy.js' ?>', 'first') ?]
 [?php use_javascript('<?php echo sfConfig::get('ua5_admin_module_web_dir', '/ua5AdminThemePlugin').'/js/ua5.js' ?>', 'first') ?]
 
 
-<?php if (isset($this->params['css']) && ($this->params['css'] !== false)): ?> 
-[?php use_stylesheet('<?php echo $this->params['css'] ?>') ?] 
+<?php if (isset($this->params['css']) && ($this->params['css'] !== false)): ?>
+[?php use_stylesheet('<?php echo $this->params['css'] ?>') ?]
 <?php endif; ?>
 
-[?php 
+[?php
 slot("list_title");
 include_partial("list_title");
 end_slot("list_title");
