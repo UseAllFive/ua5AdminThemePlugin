a1f0d5178e2b98424e0ebb819e7e49f060eec91e
Matt Farmer <matt@useallfive.com>
2011-05-11 02:29:57 +0000

    + Only add Filter toggle button if the filter form exists

--- web/js/ua5.js
+++ web/js/ua5.js
@@ -9,42 +9,45 @@ var UA5 = { };
 UA5.Filter = Class.$extend({
 
   __init__ : function($_sel) {
-  
+
     this.filter = $_sel;
     this.open = false;
-    
+
     if($($_sel)) {
-    
+
       this.addToggle();
       if(!$($_sel + ' form').hasClass('expanded_filter'))
       {
           $($_sel + ' form').hide();
       }
     }
-    
+
   },
-  
+
   addToggle : function() {
-  
-    var $self = this;
-  
-    var toggle = $('<a/>')
-      .attr({ 'class' : 'ua5_admin_filter_toggle round' })
-      .html('Open')
-      .click(function(e){
-      
-        e.preventDefault();
-    
-        if($self.open) {
-          $($self.filter + ' form').hide(); $(e.target).html('Open'); $self.open = false;
-        } else {
-          $($self.filter + ' form').show(); $(e.target).html('Close'); $self.open = true;
-        }
-    
-      });
-    
-    $(this.filter).append(toggle);
-  
+
+    var $self = this,
+        toggle;
+
+    if ( $(this.filter).find('form').length ) {
+      toggle = $('<a/>')
+        .attr({ 'class' : 'ua5_admin_filter_toggle round' })
+        .html('Open')
+        .click(function(e){
+
+          e.preventDefault();
+
+          if($self.open) {
+            $($self.filter + ' form').hide(); $(e.target).html('Open'); $self.open = false;
+          } else {
+            $($self.filter + ' form').show(); $(e.target).html('Close'); $self.open = true;
+          }
+
+        });
+
+      $(this.filter).append(toggle);
+    }
+
   }
 
 });
