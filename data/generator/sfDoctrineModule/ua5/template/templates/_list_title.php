30ad421798727bfc828830cabd9f89a4e6572282
Matt Farmer <matt@useallfive.com>
2011-05-26 23:54:15 +0000

    + Allow Page title to be overridden

--- data/generator/sfDoctrineModule/ua5/template/templates/_list_title.php
+++ data/generator/sfDoctrineModule/ua5/template/templates/_list_title.php
@@ -1,1 +1,7 @@
-<?php echo ucfirst($this->getPluralName()) ?> list
+[?php 
+if ( $sf_request->hasAttribute('list.title') ) {
+  echo $sf_request->getAttribute('list.title');
+} else {
+  echo <?php echo $this->getI18NString('list.title') ?>;
+}
+?]
