af3136c5e190c2542c00d04798c84b2797dfcc56
Matt Farmer <matt@useallfive.com>
2011-08-26 07:37:18 +0000

    + Update _site_nav template

--- modules/ua5Nav/templates/_site_nav.php
+++ modules/ua5Nav/templates/_site_nav.php
@@ -1,12 +1,30 @@
-<!--
 <ul class="nav">
-  <li class="l1 model_admin">
-    <?php echo link_to('<span class="icon"></span>Model Admin', 'model_admin/index'); ?> 
+
+
+<?php /*
+
+
+<?php
+  $modules = array(
+    'category'
+  );
+?>
+<?php foreach ( $modules as $module ) : ?>
+<?php
+        $class = Doctrine_Inflector::urlize($module);
+        if ( $sf_request->getParameter('module')==$module ) {
+          $class.=' rl c_p3 rl_5 active';
+        }
+?>
+  <li class="l1 <?php echo $class; ?>">
+    <?php echo link_to('<span class="icon"></span>Categories', 'category/index'); ?>-
   </li>
-  <li class="l1 property_admin">
-    <?php echo link_to('<span class="icon"></span>Property Admin', 'property/index'); ?> 
-  </li>
+<?php endforeach; ?>
+
+
+*/ ?>
+
+
   <li class="l1 last">
   </li>
 </ul>
--->
