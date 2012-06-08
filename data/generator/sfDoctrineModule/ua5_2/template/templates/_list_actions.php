f9d8cf50f5e54f921f4413ff467369fa7d5f1a21
Matt Farmer <matt@useallfive.com>
2012-06-08 22:38:38 +0000

    + Stricter string checking

--- data/generator/sfDoctrineModule/ua5_2/template/templates/_list_actions.php
+++ data/generator/sfDoctrineModule/ua5_2/template/templates/_list_actions.php
@@ -1,6 +1,6 @@
 <?php if ($actions = $this->configuration->getValue('list.actions')): ?>
 <?php foreach ($actions as $name => $params): ?>
-<?php if ('_new' == $name): ?>
+<?php if ('_new' === $name): ?>
 <?php echo $this->addCredentialCondition('[?php echo $helper->linkToNew('.$this->asPhp($params).') ?]', $params)."\n" ?>
 <?php else: ?>
 <li class="sf_admin_action_<?php echo $params['class_suffix'] ?>">
