3f59db1928ace8392281c3f9be22b06989d68666
Matt Farmer <matt@useallfive.com>
2012-07-18 22:37:38 +0000

    + add getGuardUserId method to ua5GuardSecurityUser

--- lib/user/ua5GuardSecurityUser.class.php
+++ lib/user/ua5GuardSecurityUser.class.php
@@ -2,6 +2,7 @@
 
 class ua5GuardSecurityUser extends sfGuardSecurityUser {
 
+
   public function addFlash($name, $value) {
     $data = $this->getFlash($name);
     if ( !is_array($data) ) {
@@ -14,5 +15,14 @@ class ua5GuardSecurityUser extends sfGuardSecurityUser {
     $data[] = $value;
     return $this->setFlash($name, $data);
   }
+
+
+  /**
+   * Get logged-in user ID without hitting the database
+   */
+  public function getGuardUserId($default = false) {
+    return $this->getAttribute('user_id', $default, 'sfGuardSecurityUser');
+  }
+
 
 }
