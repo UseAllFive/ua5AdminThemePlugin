1f997da413248a32b949fe903163944c70e55108
Matt Farmer <matt@useallfive.com>
2012-10-16 00:03:53 +0000

    + Convert debug info to output in bytes

--- lib/debug/ua5JsonDebugInfoMemory.class.php
+++ lib/debug/ua5JsonDebugInfoMemory.class.php
@@ -6,7 +6,8 @@ class ua5JsonDebugInfoMemory {
   public function jsonSerialize() {
     return array(
       'memory' => array(
-        'usage' => sprintf('%.1f', (memory_get_peak_usage(true) / 1024)),
+        'usage' => sprintf('%.1f', (memory_get_peak_usage(true))),
+        'units' => 'bytes',
       ),
     );
   }
