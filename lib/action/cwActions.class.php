821c3c60e33817b1bfb645687da3ddb97315eebd
Matt Farmer <matt@useallfive.com>
2012-10-31 22:49:43 +0000

    + Make actions follow PSR-1 and PSR-2 (as much as possible without breaking   backwards-compatibility)

--- lib/action/cwActions.class.php
+++ lib/action/cwActions.class.php
@@ -4,13 +4,11 @@
  * FIXME: Where is this used?  Nothing since at lease AIALosAngeles AFAIK ^m14t
  *
  */
-class cwActions extends sfActions {
-
-
-  public function preExecute() {
-    sfConfig::set('sf_app_template_dir', sfConfig::get("sf_plugins_dir").'/ua5AdminThemePlugin/templates');
-    parent::preExecute();
-  }
-
-
+class cwActions extends sfActions
+{
+    public function preExecute()
+    {
+        sfConfig::set('sf_app_template_dir', sfConfig::get("sf_plugins_dir").'/ua5AdminThemePlugin/templates');
+        parent::preExecute();
+    }
 }
--- lib/action/ua5Action.class.php
+++ lib/action/ua5Action.class.php
@@ -1,128 +1,132 @@
 <?php
 
-abstract class ua5Action extends sfAction {
+abstract class ua5Action extends sfAction
+{
+    /**
+     * Appends the given data as JSON to the response content and bypasses the built-in view system.
+     *
+     * This method must be called as with a return:
+     *
+     * <code>return $this->renderJson($myData)</code>
+     *
+     * @param string $json JSON to append to the response
+     *
+     * @return sfView::NONE
+     */
+    public function renderJson($json, $jsonp = false)
+    {
+        self::setJsonResponseHeaders($this->getResponse());
 
+        if (sfConfig::get('sf_debug')) {
+            $debug = array();
+            if (array_key_exists('debug', $json)) {
+                $debug = $json['debug'];
+            }
 
-  /**
-   * Appends the given data as JSON to the response content and bypasses the built-in view system.
-   *
-   * This method must be called as with a return:
-   *
-   * <code>return $this->renderJson($myData)</code>
-   *
-   * @param string $json JSON to append to the response
-   *
-   * @return sfView::NONE
-   */
-  public function renderJson($json, $jsonp = false) {
-    self::setJsonResponseHeaders($this->getResponse());
+            $debug_info = new ua5JsonDebugInfo();
 
-    if ( sfConfig::get('sf_debug') ) {
-      $debug = array();
-      if ( array_key_exists('debug', $json) ) {
-        $debug = $json['debug'];
-      }
+            $json['debug'] = array_merge(
+                $debug_info->jsonSerialize(),
+                $debug
+            );
+        }
 
-      $debug_info = new ua5JsonDebugInfo();
-
-      $json['debug'] = array_merge(
-        $debug_info->jsonSerialize(),
-        $debug
-      );
+        $output = json_encode($json);
+        if (false !== $jsonp) {
+            $output = sprintf('%s(%s)', $jsonp, $output);
+        }
+        return $this->renderText($output);
     }
 
-    $output = json_encode($json);
-    if (false !== $jsonp) {
-      $output = sprintf('%s(%s)', $jsonp, $output);
+
+    public function renderJsonSuccess($json = array(), $jsonp = false)
+    {
+        $default_data = array(
+            'status' => 'success',
+        );
+        return $this->renderJson(array_merge($default_data, $json), $jsonp);
     }
-    return $this->renderText($output);
-  }
 
 
-  public function renderJsonSuccess($json = array(), $jsonp = false) {
-    $default_data = array(
-      'status' => 'success',
-    );
-    return $this->renderJson(array_merge($default_data, $json), $jsonp);
-  }
-
-
-  public function renderJsonError($errors = array(), $jsonp = false) {
-    $data = array(
-      'status' => 'error',
-      'errors' => $errors
-    );
-    return $this->renderJson($data, $jsonp);
-  }
-
-
-  public function setResponseCacheDuration($timeout) {
-    $response = $this->getResponse();
-
-    if ( false === $timeout || 0 === $timeout ) {
-      // prevent response caching on client side
-      $response->addCacheControlHttpHeader('no-cache, must-revalidate');
-      $response->setHttpHeader('Expires', 'Mon, 26 Jul 1997 05:00:00 GMT');
-    } else {
-      //-- References:
-      //   https://developers.google.com/speed/docs/best-practices/caching
-      //
-      /* */
-      //-- What it seems like we should send
-      $response->addCacheControlHttpHeader(sprintf('max-age=%s, public, must-revalidate', $timeout));
-      $response->setHttpHeader('Expires', gmdate('D, j M Y H:i:s \G\M\T', time()+$timeout));
-      $response->setHttpHeader('Last-modified', gmdate('D, j M Y H:i:s \G\M\T', time()));
-      header_remove('Pragma');
-      /* */
-
-      /*
-      //-- Attempts to make it work
-      $response->addCacheControlHttpHeader(sprintf('max-age=%s, public', $timeout));
-      $response->setHttpHeader('Expires', gmdate('D, j M Y H:i:s \G\M\T', time()+$timeout));
-      $response->setHttpHeader('Last-modified', 'Mon, 26 Jul 1997 05:00:00 GMT');
-      //$response->setHttpHeader('Pragma', 'public');
-      header_remove('Cookie');
-      header_remove('Set-Cookie');
-      /* */
+    public function renderJsonError($errors = array(), $jsonp = false)
+    {
+        $data = array(
+            'status' => 'error',
+            'errors' => $errors
+        );
+        return $this->renderJson($data, $jsonp);
     }
-    return $response;
-  }
 
 
-  static public function setJsonResponseHeaders(sfResponse $response) {
-    $response->setContentType('application/json');
+    public function setResponseCacheDuration($timeout)
+    {
+        $response = $this->getResponse();
 
-    return $response;
-  }
+        if ( false === $timeout || 0 === $timeout ) {
+            // prevent response caching on client side
+            $response->addCacheControlHttpHeader('no-cache, must-revalidate');
+            $response->setHttpHeader('Expires', 'Mon, 26 Jul 1997 05:00:00 GMT');
+        } else {
+            //-- References:
+            //   https://developers.google.com/speed/docs/best-practices/caching
+            //
+            /* */
+            //-- What it seems like we should send
+            $response->addCacheControlHttpHeader(sprintf('max-age=%s, public, must-revalidate', $timeout));
+            $response->setHttpHeader('Expires', gmdate('D, j M Y H:i:s \G\M\T', time()+$timeout));
+            $response->setHttpHeader('Last-modified', gmdate('D, j M Y H:i:s \G\M\T', time()));
+            header_remove('Pragma');
 
-
-  /**
-   * Forwards current action to the default 403 error action unless the specified condition is true.
-   *
-   * @param bool    $condition  A condition that evaluates to true or false
-   *
-   */
-  public function forward403Unless($condition) {
-    if (!$condition) {
-      $this->forward(sfConfig::get('sf_secure_module'), sfConfig::get('sf_secure_action'));
+            /*
+            //-- Attempts to make it work
+            $response->addCacheControlHttpHeader(sprintf('max-age=%s, public', $timeout));
+            $response->setHttpHeader('Expires', gmdate('D, j M Y H:i:s \G\M\T', time()+$timeout));
+            $response->setHttpHeader('Last-modified', 'Mon, 26 Jul 1997 05:00:00 GMT');
+            //$response->setHttpHeader('Pragma', 'public');
+            header_remove('Cookie');
+            header_remove('Set-Cookie');
+            */
+        }
+        return $response;
     }
-  }
 
- /**
-  * Generic form error output.
-  */
-  protected function renderFormJsonError(sfFormDoctrine $form) {
-    $errors = array();
-    foreach ($form->getFormFieldSchema() as $name => $field) {
-      //-- Taken from `sfFormField::renderError`.
-      $error = $field->getWidget() instanceof sfWidgetFormSchema
-        ? $field->getWidget()->getGlobalErrors($field->getError())
-        : $field->getError();
-      if ( null !== $error ) {
-        $errors[] = $error->getMessage();
-      }
+
+    public static function setJsonResponseHeaders(sfResponse $response)
+    {
+        $response->setContentType('application/json');
+
+        return $response;
     }
-    return $this->renderJsonError($errors);
-  }
 
+
+    /**
+     * Forwards current action to the default 403 error action unless the specified condition is true.
+     *
+     * @param bool    $condition  A condition that evaluates to true or false
+     *
+     */
+    public function forward403Unless($condition)
+    {
+        if (!$condition) {
+            $this->forward(sfConfig::get('sf_secure_module'), sfConfig::get('sf_secure_action'));
+        }
+    }
+
+    /**
+     * Generic form error output.
+     */
+    protected function renderFormJsonError(sfFormDoctrine $form)
+    {
+        $errors = array();
+        foreach ($form->getFormFieldSchema() as $name => $field) {
+            //-- Taken from `sfFormField::renderError`.
+            $error = $field->getWidget() instanceof sfWidgetFormSchema
+                ? $field->getWidget()->getGlobalErrors($field->getError())
+                : $field->getError();
+            if (null !== $error) {
+                $errors[] = $error->getMessage();
+            }
+        }
+        return $this->renderJsonError($errors);
+    }
 }
